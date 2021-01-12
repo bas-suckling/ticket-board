2af1c3022d149c1ce80c970740dfdfb04d67cb56
bas.suckling <suckling.w.s@gmail.com>
2021-01-13 10:30:35 +1300

    add material-ui table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import TicketBoard from './TicketBoard'
 
 function App() {
   return (
     <div className="App">
-      <h1>Ticket Board</h1>
+      <h1>Ticket Board Application</h1>
+      <TicketBoard/>
     </div>
   );
 }
