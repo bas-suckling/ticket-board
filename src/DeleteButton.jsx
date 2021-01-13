f4e29fb9857968daf4ab3a561d60853e2b84203f
bas.suckling <suckling.w.s@gmail.com>
2021-01-14 08:51:50 +1300

    refactor delete functionality into DeleteButton

---
+++ src/DeleteButton.jsx
@@ -0,0 +1,29 @@
+import { useMutation } from "@apollo/client";
+import { DELETE_TICKET } from "./graphql/mutations/deleteTicket";
+import { organisationId } from './organisation'
+
+function DeleteButton({id, board}) {
+
+    const [deleteTicket, state] = useMutation(DELETE_TICKET, {
+        variables: { organisationId, ticketId: id },
+        update: (cache) => {
+            cache.modify({
+                id: cache.identify(board),
+                fields: {
+                    tickets(existingTickets, {readField}) {
+                        console.log(state)
+                        return existingTickets.filter((ticketRef) => readField("id", ticketRef) !== id)
+                    }
+                }
+            })
+        }
+    });
+
+    return(
+        <>
+            <button onClick={() => deleteTicket()}>Delete</button>
+        </>
+    )
+}
+
+export default DeleteButton
--- src/Ticket.jsx
+++ src/Ticket.jsx
@@ -1,35 +1,15 @@
-import { useMutation } from "@apollo/client";
-import { DELETE_TICKET } from "./graphql/mutations/deleteTicket";
-import { organisationId } from './organisation'
+import DeleteButton from './DeleteButton'
 
 
 function Ticket({ board, ticket }) {
     const { id, name, description, status } = ticket;
 
-
     // add ticket should add the result to the tickets field, instead of removing the exisitng one, will sit one level higher on the board
     // edit ticket does not need an update method as apollo knows based on the ID
 
-
-    const [deleteTicket, state] = useMutation(DELETE_TICKET, {
-        variables: { organisationId, ticketId: id },
-        update: (cache) => {
-            cache.modify({
-                id: cache.identify(board),
-                fields: {
-                    tickets(existingTickets, {readField}) {
-                        return existingTickets.filter((ticketRef) => readField("id", ticketRef) !== id)
-                    }
-                }
-            })
-        }
-    });
-
-    console.log(state);
-
     return (
-        <div className="ticket">
-            {name}, {description}, {status} <button onClick={() => deleteTicket()}>Delete</button>
+        <div className="ticket" id={id}>
+            {name}, {description}, {status} <DeleteButton id={id} board={board} />
         </div>
     );
 }
