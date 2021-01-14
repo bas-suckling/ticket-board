d692f26e81dac9e452370b0914db7d377d3a9c3e
bas.suckling <suckling.w.s@gmail.com>
2021-01-15 09:34:29 +1300

    debugging cache issues

--- src/Components/NewTicketForm.jsx
+++ src/Components/NewTicketForm.jsx
@@ -4,49 +4,37 @@ import { organisationId } from '../organisation'
 import { useInput } from './hooks/useInput'
 
 
+
 function NewTicketForm({ board }) {
     const { value: name, bind: bindName, reset: resetName } = useInput('');
     const { value: description, bind: bindDescription, reset: resetDescription } = useInput('');
     const { value: status, bind: bindStatus, reset: resetStatus } = useInput('TODO');
     let visible = true
 
-    const [newTicket] = useMutation(PUT_TICKET, {
-        update: (cache) => {
-            cache.modify({
-                id: cache.identify(board),
-                fields: {
-                    tickets(existingTickets, {readField}) {
-                        return existingTickets.add((ticketRef) => readField("id", ticketRef))
-                    }
-                }
-            })
+    const [newTicket] = useMutation(PUT_TICKET)
+
+            return (
+                <form onSubmit={e => {
+                    e.preventDefault()
+                    newTicket({ variables: { organisationId, boardId: board.id, input: { name, description, status, visible } } })
+                    resetName()
+                    resetDescription()
+                    resetStatus()
+                }}>
+                    <label>Name</label>
+                    <input name="name" required {...bindName} />
+                    <label>Description</label>
+                    <input name="description" required {...bindDescription} />
+                    <label>Status</label>
+                    <select name="status" required {...bindStatus}>
+                        <option value="TODO">TODO</option>
+                        <option value="INPROGRESS">INPROGRESS</option>
+                        <option value="DONE">DONE</option>
+                    </select>
+                    <input type="submit" value="Submit" />
+                </form>
+            )
+
         }
-    })
-
-    
-
-    return (
-        <form onSubmit={e => {
-            e.preventDefault()
-            newTicket({ variables: { organisationId, boardId: board.id, input: { name, description, status, visible } } })
-            resetName()
-            resetDescription()
-            resetStatus()
-        }}>
-            <label>Name</label>
-            <input name="name" required {...bindName} />
-            <label>Description</label>
-            <input name="description" required {...bindDescription} />
-            <label>Status</label>
-            <select name="status" required {...bindStatus}>
-                <option value="TODO">TODO</option>
-                <option value="INPROGRESS">INPROGRESS</option>
-                <option value="DONE">DONE</option>
-            </select>
-            <input type="submit" value="Submit" />
-        </form>
-    )
-
-}
 
 export default NewTicketForm
